3d5f2c3327dbf34a6518bad38f1f3090046ae14a
Raju Mollick <x1litecheat@gmail.com>
2026-01-20 19:37:57 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Frontend configuration used by the static Vercel site.
+// Update apiBaseUrl to point at the Python API server.
+window.APP_CONFIG = {
+  apiBaseUrl: "http://85.215.159.4:11420",
+  assetBaseUrl: "http://85.215.159.4:11420", // optional: where static assets (logo/style) are served from
+  appName: "EMS TOGGLE DUTY",
+  companyName: "BPR"
+};
